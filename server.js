b92c9d598662c1fa23fbd8fdcc9f00e04cac9d12
Elwood Berry <me@elwoodberry.com>
2017-04-28 06:05:16 -0400

    Updated the server.js

--- server.js
+++ server.js
@@ -1,28 +1,42 @@
 // Dependencies
-var express = require('express');  
-var path = require('path');  
-var bodyParser = require('body-parser'); 
+var express = require('express');
+var app = express(); 
+var bodyParser = require('body-parser');
+// var path = require('path'); 
 
 // Routes
-var index = require('./routes/index');
+// var index = require('./routes/index');
 
 // Port
-var port = 8080;
+// var port = 8080;
 
 // Express
-var app = express(); 
+
 
 // Body Parser
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json()); 
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
 
 // Static Route Angular
-app.use(express.static(path.join(__dirname, 'client/dist')));
+// app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // Routes
-app.use('/', index);
+// app.use('/', index);
+
+
+// application -------------------------------------------------------------
+app.get('/', function(req, res) {
+	res.sendfile('client/dist/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 // Listen to run the server
-app.listen(port, function(){
-	console.log('Available On Port: ' + port);
-});
+// app.listen(port, function(){
+	// console.log('Available On Port: ' + port);
+// });
+
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
