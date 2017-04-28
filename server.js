5dd08a66ae98c6291d91b4d42e799759a032e1bd
Elwood Berry <me@elwoodberry.com>
2017-04-28 12:40:36 -0400

    Updates to server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Port
-var port;
+var port = 8080;
 
 // Route
 // app.get('/', function(req, res){res.send('working');});
