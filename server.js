40ce0ff9e930623df646513b4a97a71fb4552e3b
Elwood Berry <me@elwoodberry.com>
2017-05-01 07:34:50 -0400

    Updated the port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Port
-var port = 8080;
+var port = 80;
 
 // Route
 // app.get('/', function(req, res){res.send('working');});
