16a619e22d4312a764d46b54f130e79c19d030a0
Elwood Berry <me@elwoodberry.com>
2017-05-19 03:07:56 -0400

    updated port

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     sass = require('gulp-sass'),
-    cleanCss = require('gulp-clean-css'),
+    minifyCss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     paths = {sass: ['./scss/**/*.scss']};
 
@@ -11,12 +11,12 @@ gulp.task('sass', function(done) {
   gulp.src('./scss/styles.scss')
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./client/'))
-    .pipe(cleanCss({
+    .pipe(gulp.dest('./client/src'))
+    .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./client/'))
+    .pipe(gulp.dest('./client/src'))
     .on('end', done);
 });
 
